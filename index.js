087ae746aba94994c38cad9cf8911ec748f3d47b
server_42 <devbhattacharya42@gmail.com>
2023-09-06 13:09:16 +0530

    Understanding schemas and read and write operations

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 const mongoose = require('mongoose')
-const blogSchema = new mongoose.Schema({
-    author : {type: String , required: true },
-    title : String,
-    createAt: Date,
-    content: String,
-    tags: [String]
+// Since we are connecting our database(MongoDB) to our server(Node + Express) we are working with a asyncrounous way
+// Creating data
+const userSchema = mongoose.Schema({
+    name : {type : String , validation : true , unique : true},
+    age : {type : Number , default : 18},
+    country : {type : String , required : true, enum : ["USA","India","Maldives"],default : "India"}
 })
-const Blog = mongoose.model('blog',blogSchema)
-const main = async () => {
-    try{
-    const conn = await mongoose.connect("mongodb://localhost:27017/website")
-    
-    console.log("Connected")
-    
-    // const blog = new Blog({
-    //     author: "Jenna Doe",
-    //     title: "Learn Mongo",
-    //     createdAt: new Date(),
-    //     content: "Mongo is good...",
-    //     tags: ["mongo","tech"]
-    // })
-    const data = await Blog.find().sort({title: -1})    
-    console.log(data)
-    // await blog.save()
+const UserModule = mongoose.model('user',userSchema)
 
-    conn.disconnect()
+const main = async() => {
+    try{
+        const connection = await mongoose.connect('mongodb://127.0.0.1:27017/mgnclass')
+        console.log("Connected to database")
+        // Inserting data is so simple by creating schemas
+        await UserModule.insertMany([{name : "Arbaz"}])
+        console.log("Data inserted")
+        // It's so simple to read data
+        // const data = await UserModule.find()
+        // console.log(data)
+        connection.disconnect()
     }catch(e){
+        console.log("Error connecting to DB")
         console.log(e)
     }
+    
 }
 main()
